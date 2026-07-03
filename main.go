8a758a17b2083ee38e209948fc35afebaf4d6393
MykyZhu <mykyaltfortesting@gmail.com>
2026-07-03 11:04:57 +0300

    added gorelease files

--- main.go
+++ main.go
@@ -8,24 +8,32 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+// Version is set at build time by GoReleaser.
+var Version = "dev"
+
 func main() {
-	var model tui.Model
-
-	// If a log file path is passed as an argument, open it directly in the viewer
 	if len(os.Args) > 1 {
-		path := os.Args[1]
-		m, err := tui.NewWithFile(path)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
-			os.Exit(1)
+		switch os.Args[1] {
+		case "--version", "-v":
+			fmt.Println("site-audit", Version)
+			return
+		default:
+			// Treat as a log file path to open directly
+			m, err := tui.NewWithFile(os.Args[1])
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+				os.Exit(1)
+			}
+			run(m)
+			return
 		}
-		model = m
-	} else {
-		model = tui.New()
 	}
+	run(tui.New())
+}
 
+func run(m tui.Model) {
 	p := tea.NewProgram(
-		model,
+		m,
 		tea.WithAltScreen(),
 		tea.WithMouseCellMotion(),
 	)
